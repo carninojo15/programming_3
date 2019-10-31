5b7010165d9ae7b07cc13f744cc7f78d446326cc
Emily Costa <ecost020@fiu.edu>
2019-10-31 13:12:09 -0400

    fixing bugs

--- hw1/wcdigits/wcdigits.c
+++ hw1/wcdigits/wcdigits.c
@@ -3,11 +3,30 @@
  #define OUT 0 /* outside a word */
  /* count lines, words, and characters in input */
 int main(){
+   FILE* fp;
+   char digits[10];
+   char filename[100]; 
+   printf("Enter file name: "); 
+   scanf("%s", filename);
+   fp = fopen(filename, "r");
    int c, nl, nw, nc, state;
+   int zero, one, two, three, four, five, six, seven, eight, nine;
    state = OUT;
    nl = nw = nc = 0;
-   while ((c = getchar()) != EOF) {
+   while ((c = getc(fp)) != EOF) {
+      printf("%c",c);
       ++nc;
+      if(c>='0' && c<='9') digits[c+'0']++;
+      /*if (c == ('0'-'0')) ++digits[0];
+      if (c == '1') ++one;
+      if (c == '2') ++two;
+      if (c == '3') ++three;
+      if (c == '4') ++four;
+      if (c == '5') ++five;
+      if (c == '6') ++six;
+      if (c == '7') ++seven;
+      if (c == '8') ++eight;
+      if (c == '9') ++nine;*/ 
       if (c == '\n')
          ++nl;
       if (c == ' ' || c == '\n')
@@ -18,4 +37,6 @@ int main(){
       }
    }
    printf("%d %d %d\n", nl, nw, nc);
+   for(int i=0;i<10;i++) printf("%d, %d\n", i, digits[i]);
+   return 0;
 }
