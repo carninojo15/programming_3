41fcadf5f47e00ee7dd5f12b1442aa0c37dfbe75
Emily Costa <ecost020@fiu.edu>
2019-10-31 16:44:10 -0400

    final edits

--- hw1/wcdigits/wcdigits.c
+++ hw1/wcdigits/wcdigits.c
@@ -3,33 +3,20 @@
  #define OUT 0 /* outside a word */
  /* count lines, words, and characters in input */
 int main(){
-   FILE* fp;
-   char digits[10];
-   char filename[100]; 
-   printf("Enter file name: "); 
-   scanf("%s", filename);
-   fp = fopen(filename, "r");
+   int digits[10];
+   for (int i = 0; i < 10; ++i) digits[i] = 0;
    int c, nl, nw, nc, state;
-   int zero, one, two, three, four, five, six, seven, eight, nine;
    state = OUT;
    nl = nw = nc = 0;
-   while ((c = getc(fp)) != EOF) {
+   while ((c = getchar()) != EOF) {
       printf("%c",c);
       ++nc;
-      if(c>='0' && c<='9') digits[c+'0']++;
-      /*if (c == ('0'-'0')) ++digits[0];
-      if (c == '1') ++one;
-      if (c == '2') ++two;
-      if (c == '3') ++three;
-      if (c == '4') ++four;
-      if (c == '5') ++five;
-      if (c == '6') ++six;
-      if (c == '7') ++seven;
-      if (c == '8') ++eight;
-      if (c == '9') ++nine;*/ 
-      if (c == '\n')
+      if(c>='0' && c<='9'){
+      ++digits[c-'0'];
+      }
+      else if (c == '\n')
          ++nl;
-      if (c == ' ' || c == '\n')
+      if (c == ' ' || c == '\n' || c == '\t')
          state = OUT;
       else if (state == OUT) {
          state = IN;
