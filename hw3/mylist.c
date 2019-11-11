06f313fa993c1ae94b200f225584ea8977aab797
Emily Costa <ecost020@fiu.edu>
2019-11-11 18:33:43 -0500

    Started hw3

--- hw3/mylist.c
+++ hw3/mylist.c
@@ -9,74 +9,82 @@ struct node_t {
     char* str;
     struct node_t* prev; 
     struct node_t* next; 
-}
+} *head, *tail;
+
+//struct node_t (*head) = NULL; 
+//struct node_t (*tail) = NULL; 
 
 void insert(char* str){
-    char* str, data;
-    struct node_t *newNode;
-    /* find where to insert new node
-    use strcmp() to determine increasing order*/
-    while(strcomp(str,temp))
-
-    if(n >= 1)
-    {
-        head = (struct node *)malloc(sizeof(struct node));
-
-        if(head != NULL)
-        {
-            printf("Enter data of 1 node: ");
-            scanf("%d", &data);
-
-            head->data = data;
-            head->prev = NULL;
-            head->next = NULL;
-
-            last = head;
-
-            /*
-             * Create rest of the n-1 nodes
-             */
-            for(i=2; i<=n; i++)
-            {
-                newNode = (struct node *)malloc(sizeof(struct node));
-
-                if(newNode != NULL)
-                {
-                    printf("Enter data of %d node: ", i);
-                    scanf("%d", &data);
-
-                    newNode->data = data;
-                    newNode->prev = last; // Link new node with the previous node
-                    newNode->next = NULL;
-
-                    last->next = newNode; // Link previous node with the new node
-                    last = newNode;          // Make new node as last/previous node
-                }
-                else
-                {
-                    printf("Unable to allocate memory.");
-                    break;
-                }
-            }
-
-            printf("\nDOUBLY LINKED LIST CREATED SUCCESSFULLY\n");
-        }
-        else
-        {
-            printf("Unable to allocate memory");
-        }
+    struct node_t* newNode = (struct node_t*)malloc(sizeof(struct node_t));
+    newNode->str = str;
+    newNode->next = NULL;
+    
+    // For first node in DLL
+    if(head==NULL){
+        head = str;
+        tail = str;
+        head->prev = NULL;
+        return;
     }
+
+    // If node has less value than head
+    if(strcmp(newNode->str,head->str) < 0){ 
+        newNode->prev = NULL; 
+        head->prev = newNode; 
+        newNode->next = head; 
+        head = newNode; 
+        return; 
+    } 
+
+    // If node has more value than tail
+    if(strcmp(newNode->str,tail->str) > 0){
+        newNode->prev = tail; 
+        tail->next = newNode; 
+        tail = newNode; 
+        return; 
+    }
+
+    // Find the node before which we need to insert newNode. 
+    struct node_t *temp = head->next; 
+    while(strcmp(temp->str,newNode->str) < 0)
+        temp = temp->next; 
+  
+    // Insert new node before temp 
+    (temp->prev)->next = newNode; 
+    newNode->prev = temp->prev; 
+    temp->prev = newNode; 
+    newNode->next = temp; 
 }
 
 void delete(char* str)
 {
+    struct node_t *temp = head;
 
+    if (head == NULL) 
+        return;
+
+    while (temp->next != NULL){
+        if (strcmp(temp->str, str) == 0){
+            if (temp->prev != NULL){
+                temp->prev->next = temp->next;
+            }
+            if (temp->next != NULL){
+                temp->next->prev = temp->prev;
+            }
+            if (head == temp){
+                head = temp->next;
+            }
+            free(temp);
+            return;
+        }
+        temp = temp->next;
+    }
 }
 
 void list(int reverse_order){
     /*print from beginning to end*/
     if(reverse_order==0){
-        struct node * temp;
+        struct node_t *temp = (struct node_t*)malloc(sizeof(struct node_t));
         int n = 1;
 
         if(head == NULL){
@@ -87,7 +95,7 @@ void list(int reverse_order){
             printf("\n\nDATA IN THE LIST:\n");
 
             while(temp != NULL){
-                printf("DATA of %d node = %d\n", n, temp->data);
+                printf("DATA of %d node = %d\n", n, temp->str);
                 n++;
                 /* Move the current pointer to next node */
                 temp = temp->next;
@@ -97,18 +105,18 @@ void list(int reverse_order){
 
     /*print from end to beginning*/
     else{
-        struct node * temp;
+        struct node_t *temp = (struct node_t*)malloc(sizeof(struct node_t));
         int n = 0;
 
-        if(last == NULL){
+        if(tail == NULL){
             printf("List is empty.");
         }
         else{
-            temp = last;
+            temp = tail;
             printf("\n\nDATA IN THE LIST:\n");
 
             while(temp != NULL){
-                printf("DATA of last-%d node = %d\n", n, temp->data);
+                printf("DATA of last-%d node = %d\n", n, temp->str);
                 n++;
                 /* Move the current pointer to previous node */
                 temp = temp->prev; 
