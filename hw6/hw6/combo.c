de3de455c47567ed834a62ab74355aafb1ac1deb
Emily Costa <ecost020@fiu.edu>
2019-12-02 10:59:44 -0500

    Edits to makefile and c code

--- hw6/hw6/combo.c
+++ hw6/hw6/combo.c
@@ -3,36 +3,81 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-//execlp("/usr/bin/sort", "sort", "<", argv[0], NULL); < arg1
+//execlp("/usr/bin/sort", "sort", "<", argv[0], NULL); 
 //execlp("/bin/grep", "grep", "include", NULL);
-//execlp("usr/bin/wc", "wc", NULL); > arg2
+//execlp("usr/bin/wc", "wc", ">", argv[1], NULL); 
 
-main(int argc, char* argv []){
-    if(argc != 2){
+int fd1[2], fd2[2];
+pid_t pid;
+
+void cmd0(char* input_file){
+    printf("doing cmd0\n");
+    close(fd1[0]); // close unused pipe
+    dup2(fd1[1], 1);
+    close(fd1[1]);
+    execlp("/usr/bin/sort", "sort <", input_file, NULL);
+    fprintf(stderr, "Sort not executed properly\n");
+    exit(-1);
+}
+
+void cmd1(){
+    printf("doing cmd1\n");
+    close(fd1[1]);
+    close(fd2[0]);
+    dup2(fd1[0], 0);
+    dup2(fd2[1], 1);
+    close(fd1[0]);
+    close(fd2[1]);
+    execlp("/bin/grep", "grep", "include", NULL);
+    fprintf(stderr, "grep not executed properly\n");
+    exit(-1);
+}
+
+void cmd2(char* output_file){
+    printf("doing cmd2\n");
+    close(fd2[1]);
+    dup2(fd2[0], 0);
+    close(fd2[0]);
+    execlp("usr/bin/wc", "wc >", output_file, NULL); 
+    fprintf(stderr, "wc not executed properly\n");
+    exit(-1);
+}
+
+int main(int argc, char** argv []){
+    if(argc != 3){
         fprintf(stderr, "Expected format: ./combo input_file.c output_file");
         exit(-1);
     }
-    
-    int fd[2];
-    pid_t pid;
-    pipe(fd); // file descriptors set fd[1] for writing
+
+    pipe(fd1); // file descriptors set fd[1] for writing
+    pipe(fd2);
     pid = fork(); 
+    char *ch0 = argv[0];
+    char *ch1 = argv[1];
 
-    if(pid < 0) {
-        fprintf(stderr, "Fork Failed");
-        exit(-1);
-    }
+    if ((pid = fork()) == -1) {
+        perror("bad fork1");
+        exit(1);
+    } 
+    else if (pid == 0)
+        cmd0(ch0); // stdin -> sort ->  fd1
 
-    if(pid > 0){ // parent process (sort)
-        close(fd[0]);
-        // direct output to file desrciptor for child
-        dup(fd[1]);
-        execlp("/usr/bin/sort", "sort", "<", argv[0], NULL);
-    }
-    else if(pid == 0){ //child process
-        close(fd[1]);
-        dup(fd[0]);
-        execlp("/bin/grep", "grep", "include", NULL);
-    }
+    if ((pid = fork()) == -1) {
+        perror("bad fork2");
+        exit(1);
+    } 
+    else if (pid == 0) 
+        cmd1(); // fd1 -> grep -> fd2
+
+    close(fd1[0]);
+    close(fd1[1]);
+
+    if ((pid = fork()) == -1) {
+        perror("bad fork3");
+        exit(1);
+    }  
+    else if (pid == 0) 
+        cmd2(ch1); // fd2 -> wc -> stdout (the output file)
     return 0;
 }
+
